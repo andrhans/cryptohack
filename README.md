917d4b3d7aed169767b51dd9289d026fb0ce05dd
andrhans <andreahm@gmx.com>
2023-04-06 09:14:51 +0200

    adding readme and hex challenge

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# cryptohack
+
+![cryptohack](main.png)
+
+Repo containing the basic solutions made during cryptohack challenges
+
+
+
+
---
+++ hex_to_bytes.py
@@ -0,0 +1,6 @@
+
+hex_str='63727970746f7b596f755f77696c6c5f62655f776f726b696e675f776974685f6865785f737472696e67735f615f6c6f747d'
+
+hex_to_bytes=bytes.fromhex(hex_str)
+
+print(hex_to_bytes)
